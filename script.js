6d9a4c95e99f7d9ef3788e848a7ecb22db4bbb3c
Lesirec <100166141+Lesirec@users.noreply.github.com>
2025-04-01 01:25:25 +0300

    script.js

--- script.js
+++ script.js
@@ -1,22 +1,18 @@
+/* scripts.js */
 let currentIndex = 0;
-const slides = document.querySelectorAll('.slide');
-const carousel = document.querySelector('.carousel');
-
-function nextSlide() {
+const slides = document.querySelectorAll(".slide");
+setInterval(() => {
+    slides[currentIndex].classList.remove("active");
     currentIndex = (currentIndex + 1) % slides.length;
-    carousel.style.transform = `translateX(-${currentIndex * 100}%)`;
-}
+    slides[currentIndex].classList.add("active");
+}, 3000);
 
-setInterval(nextSlide, 3000);
-
-window.addEventListener('scroll', () => {
-    const promo = document.querySelector('.promo-container');
-    const searchBar = document.querySelector('.search-bar');
-    if (window.scrollY > 50) {
-        promo.style.opacity = '0';
-        searchBar.style.display = 'block';
+window.addEventListener("scroll", () => {
+    const searchBar = document.querySelector(".search-bar");
+    if (window.scrollY > 100) {
+        searchBar.style.display = "block";
     } else {
-        promo.style.opacity = '1';
-        searchBar.style.display = 'none';
+        searchBar.style.display = "none";
     }
 });
+
