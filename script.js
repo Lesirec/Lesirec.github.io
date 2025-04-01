8903778f0f77b66ea05f0dfa94d930937d7f6405
Lesirec <100166141+Lesirec@users.noreply.github.com>
2025-04-02 01:45:59 +0300

    script.js

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
-/* scripts.js */
 let currentIndex = 0;
 const slides = document.querySelectorAll(".slide");
+
 setInterval(() => {
     slides[currentIndex].classList.remove("active");
     currentIndex = (currentIndex + 1) % slides.length;
     slides[currentIndex].classList.add("active");
 }, 3000);
 
+// Появление строки поиска при прокрутке
 window.addEventListener("scroll", () => {
     const searchBar = document.querySelector(".search-bar");
     if (window.scrollY > 100) {
